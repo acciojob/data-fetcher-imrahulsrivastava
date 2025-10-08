c4050e922341eb13aeb092e863760e6ac7ffa4b2
imrahulsrivastava <imrahulsrivastava1@gmail.com>
2025-10-08 12:08:54 +0530

    submission

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect, useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [data, setData] = useState(null);
 
-export default App
+  useEffect(() => {
+    fetch("https://dummyjson.com/products")
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((err) => console.error("Error fetching data:", err));
+  }, []);
+
+  if (!data) return <h1>Loading...</h1>;
+
+  return <pre>{JSON.stringify(data, null, 2)}</pre>;
+};
+
+export default App;
